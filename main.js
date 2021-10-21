a3be092df6d361fe038d8eed8ab4f87bfea263a7
dsulliv4 <dsulliv4@binghamton.edu>
2021-10-20 23:14:26 -0400

    js controls functioality

---
+++ main.js
@@ -0,0 +1,58 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const halt = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+
+function toggleVideoStatus() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+
+}
+
+// update the play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+  }
+
+
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  return true;
+}
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+halt.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
+
+
+
+
